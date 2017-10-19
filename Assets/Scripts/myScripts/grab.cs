b56ec1d3d039ad197fb424c0ab6f0758a8111158
airwoo <brandontwoo96@gmail.com>
2017-10-19 01:39:29 -0400

    fixed arm bug, can toggle ik with 1 and 2 key

--- Assets/Scripts/myScripts/grab.cs
+++ Assets/Scripts/myScripts/grab.cs
@@ -14,6 +14,11 @@ public class grab : MonoBehaviour {
 
 	Animator animator;
 	Transform Zoe;
+
+	public bool ikActive = false;
+	public Transform rightHandObj = null;
+	public Transform lookObj = null;
+
 	private readonly int iG = Animator.StringToHash("isGrab");
 	static readonly Vector3 offset = new Vector3(0.2f, .04f, 0);
 
@@ -43,18 +48,52 @@ public class grab : MonoBehaviour {
 	void Update(){
 		
 		if (Input.GetKeyDown ("g") == true) {
+			
 			animator.SetBool (iG, true);
 			myRightMiddleFingerPosition = myRightHandMiddleFinger.position;
 			myRightMiddleFingerRotation = myRightHandMiddleFinger.rotation;
 			myRightHandPosition = myRightHand.position;
 
 		} 
+		if (Input.GetKeyDown ("1") == true) {
+			ikActive = true;
+
+		} 
+		if (Input.GetKeyDown ("2") == true) {
+			ikActive = false;
+
+		} 
 	}
 	void OnAnimatorIK(){
-		Vector3 targetPosition = target.transform.position;
-		targetPosition += myRightMiddleFingerPosition + myRightMiddleFingerRotation * offset - myRightHandPosition;
-		animator.SetIKPosition (AvatarIKGoal.RightHand, targetPosition);
-		animator.SetIKPositionWeight (AvatarIKGoal.RightHand, percentComplete);
+		if(animator) {
+
+			//if the IK is active, set the position and rotation directly to the goal. 
+			if(ikActive) {
+
+				// Set the look target position, if one has been assigned
+				if(lookObj != null) {
+					animator.SetLookAtWeight(1);
+					animator.SetLookAtPosition(lookObj.position);
+				}    
+
+				// Set the right hand target position and rotation, if one has been assigned
+				if(rightHandObj != null) {
+					Vector3 targetPosition = target.transform.position;
+					targetPosition += myRightMiddleFingerPosition + myRightMiddleFingerRotation * offset - myRightHandPosition;
+					animator.SetIKPosition (AvatarIKGoal.RightHand, targetPosition);
+					animator.SetIKPositionWeight (AvatarIKGoal.RightHand, percentComplete);
+				}        
+
+			}
+
+			//if the IK is not active, set the position and rotation of the hand and head back to the original position
+			else {          
+				animator.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
+				animator.SetIKRotationWeight(AvatarIKGoal.RightHand,0); 
+				animator.SetLookAtWeight(0);
+			}
+		}
+
 
 	}
 }
