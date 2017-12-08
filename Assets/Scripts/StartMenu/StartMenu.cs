c6c94784fac5dbd775296c568aff027ec53d3584
airwoo <brandontwoo96@gmail.com>
2017-12-08 18:40:47 -0500

    Start Menu
    added start menu

---
+++ Assets/Scripts/StartMenu/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour {
+
+	public GameObject mainMenu;
+	public GameObject controlMenu;
+
+	public void Play(){
+		SceneManager.LoadScene ("GameDemo");
+	}
+
+	public void MainMenu(){
+		mainMenu.SetActive (true);
+		controlMenu.SetActive (false);
+	}
+
+	public void ControlsMenu(){
+		mainMenu.SetActive (false);
+		controlMenu.SetActive (true);
+	}
+}
