36fe51acc7f9fa3465dd5ffe6a7ff7df35952450
airwoo <brandontwoo96@gmail.com>
2017-12-08 15:59:04 -0500

    early stages dialogue

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+	private GameObject triggeringNpc;
+	private bool triggering;
+
+	public GameObject npcText;
+	public Text changeText;
+
+	void Start(){
+
+	}
+
+	void Update(){
+		if (triggering) {
+			//print ("Player is triggering with " + triggeringNpc);
+			npcText.SetActive (true);
+			if(Input.GetKeyDown(KeyCode.T)){
+				changeText.text = "Hello hero! Follow me! A mother needs help!";
+			}
+		} else {
+			npcText.SetActive (false);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "NPC1") {
+			triggering = true;
+			triggeringNpc = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "NPC1") {
+			triggering = false;
+			triggeringNpc = null;
+			changeText.text = "Press T to Talk";
+		}
+	}
+
+}
