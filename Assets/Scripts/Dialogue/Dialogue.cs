4b8458347d7590305ddfeb64c47c8568ce70878f
airwoo <brandontwoo96@gmail.com>
2017-12-08 16:53:16 -0500

    dialogue update
    dialogue basically done

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -11,6 +11,8 @@ public class Dialogue : MonoBehaviour {
 	public GameObject npcText;
 	public Text changeText;
 
+	public int countNextDialogue = 0;
+
 	void Start(){
 
 	}
@@ -20,7 +22,33 @@ public class Dialogue : MonoBehaviour {
 			//print ("Player is triggering with " + triggeringNpc);
 			npcText.SetActive (true);
 			if(Input.GetKeyDown(KeyCode.T)){
-				changeText.text = "Hello hero! Follow me! A mother needs help!";
+				if (triggeringNpc.tag == "NPC1") {
+					if (countNextDialogue < 4) { 
+						changeText.text = "Hello hero! Follow me! A mother needs help!";
+
+					}
+					if (countNextDialogue > 3) { 
+						changeText.text = "Follow me hero! I will lead the way!";
+					}
+				}
+				if (triggeringNpc.tag == "NPC2") {
+					changeText.text = "Stop bothering me! Go away!";
+				}
+				if (triggeringNpc.tag == "NPC3") {
+					changeText.text = "Poor lady. I can't believe she lost her child to the wolves.";
+				}
+				if (triggeringNpc.tag == "NPC4") {
+					changeText.text = "I heard wolves love apples.";
+				}
+				if (triggeringNpc.tag == "NPC5") {
+					changeText.text = "I get apples every morning from the apple tree on the mountain.";
+				}
+				if (triggeringNpc.tag == "NPC6") {
+					changeText.text = "Please help me! My child was taken away by a pack of wolves! Please bring him back to me!";
+				}
+				if (triggeringNpc.tag == "NPC7") {
+					changeText.text = "I am a wizard! I love magic!";
+				}
 			}
 		} else {
 			npcText.SetActive (false);
@@ -28,17 +56,18 @@ public class Dialogue : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other){
-		if (other.tag == "NPC1") {
+		if (other.tag == "NPC1" || other.tag == "NPC2"|| other.tag =="NPC3"|| other.tag =="NPC4"||other.tag == "NPC5"|| other.tag =="NPC6"|| other.tag =="NPC7") {
 			triggering = true;
 			triggeringNpc = other.gameObject;
 		}
 	}
 
 	void OnTriggerExit(Collider other){
-		if (other.tag == "NPC1") {
+		if (other.tag == "NPC1" || other.tag =="NPC2"|| other.tag =="NPC3"||other.tag == "NPC4"|| other.tag =="NPC5"|| other.tag =="NPC6"|| other.tag =="NPC7") {
 			triggering = false;
 			triggeringNpc = null;
 			changeText.text = "Press T to Talk";
+			countNextDialogue++;
 		}
 	}
 
